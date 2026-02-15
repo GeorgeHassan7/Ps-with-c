cd82b75c8779d30217c5ef2604cdf23622069eb3
George Hassan <georgehassan246@gmail.com>
2026-02-15 17:11:05 +0300

    first step

---
+++ 07.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+
+void num_combinations(){
+    char a = '0';
+    char b;
+    char c = '0';
+    char d;
+
+    while (a <= '9'){
+        b = '0';
+        d = '0';
+
+        while (b <= '9'){
+            write(1,&c,1);
+            write(1, &d, 1);
+            write(1, " ", 1);
+            write(1, &a, 1);
+            write(1, &b, 1) ;
+            write(1, " ", 1);
+            b++;
+        }
+        a++;
+    }
+    c++;
+d++;
+}
+int main() {
+    num_combinations();
+
+    return 0;
+}
+
