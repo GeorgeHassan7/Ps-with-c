1d9e0506577281853245b966112ebf3779c1a495
George Hassan <georgehassan246@gmail.com>
2026-02-15 17:30:55 +0300

    Solution of Q06

--- 07.c
+++ 07.c
@@ -2,28 +2,44 @@
 
 void num_combinations(){
     char a = '0';
-    char b;
+    char b ;
     char c = '0';
-    char d;
+    char d ;
 
-    while (a <= '9'){
-        b = '0';
+
+    while (a <=9 , b <=9){
+        while (a <= '9'){
+        b = '1';
         d = '0';
 
-        while (b <= '9'){
-            write(1,&c,1);
-            write(1, &d, 1);
-            write(1, " ", 1);
-            write(1, &a, 1);
-            write(1, &b, 1) ;
-            write(1, " ", 1);
-            b++;
+            while (b <= '9'){
+                write(1,&c,1);
+                write(1, &d, 1);
+                write(1, " ", 1);
+                write(1, &a, 1);
+                write(1, &b, 1) ;
+                write(1, " ", 1);
+                b++;
+            }
+            a++;
         }
-        a++;
+        c++;
+        d++;
     }
-    c++;
-d++;
+    c ='0';
+    while (c <= '9'){
+        d = '0';
+        
+            while (d <= '9'){
+                write(1,&c,1);
+                write(1, &d, 1);
+                write(1, " ", 1);
+                d++;
+            }
+        c++;       
+            }
 }
+
 int main() {
     num_combinations();
 
