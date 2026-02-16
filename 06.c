ce8916d4bcec5c0d917e29651bc617bd444e15e9
GeorgeHassan7 <georgehassan246@gmail.com>
2026-02-16 16:48:40 +0300

    06.c

--- 06.c
+++ 06.c
@@ -1,46 +1,31 @@
 #include <unistd.h>
 
 void num_combinations(){
-    char a = '0';
-    char b ;
-    char c = '0';
-    char d ;
+    int a = 0;
+    int b;
+    char c;
 
+    while (a <= 98){
+        b = a + 1;
+        while (b <= 99){
+            c = (a / 10) + '0';
+            write(1, &c, 1);
+            c = (a % 10) + '0';
+            write(1, &c, 1);
+            write(1, " ", 1);
 
-        while (a <= '9'){
-        b = '1';
-        d = '0';
-
-            while (b <= '9'){
-                write(1,&c,1);
-                write(1, &d, 1);
-                write(1, " ", 1);
-                write(1, &a, 1);
-                write(1, &b, 1) ;
-                write(1, " ", 1);
-                b++;
-            }
-            a++;
+            c = (b / 10) + '0';
+            write(1, &c, 1);
+            c = (b % 10) + '0';
+            write(1, &c, 1);
+            write(1, " ", 1);
+            
+            b++;
         }
-        c++;
-        d++;
-    
-    c ='0';
-    while (c <= '9'){
-        d = '0';
-        
-            while (d <= '9'){
-                write(1,&c,1);
-                write(1, &d, 1);
-                write(1, " ", 1);
-                d++;
-            }
-        c++;       
-            }
+        a++;
+    }
 }
-
-int main() {
+int main(void){
     num_combinations();
-
-    return 0;
+    return (0);
 }
